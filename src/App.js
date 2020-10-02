a95e0384b7dc57bb542d822ae48ab8c763b0b20b
Rafal Fuz <rafalfuz@gmail.com>
2020-10-02 15:26:19 +0200

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
-function App() {
+const Banner = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="banner">
+      <img
+        className="banner__img"
+        src="https://placeimg.com/640/480/people"
+        alt="img"
+      ></img>
+      <p>2 / 10 / 2020</p>
+      <h1>Gigantyczny tytul zrobiony fajnym fontem</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis
+        diam et eros fringilla commodo. Quisque dapibus lectus et sem sodales,
+        quis sagittis erat pellentesque. Nullam iaculis non urna in fringilla.
+        Nulla maximus quam eget rutrum auctor.
+      </p>
+      <a href="/toarticle">Dowiedz się wiecej...</a>
     </div>
   );
+};
+
+const MainArticle = () => {
+  return (
+    <div className="main-article">
+      <img
+        className="main-article__img"
+        src="https://placeimg.com/640/480/tech"
+        alt="img"
+      ></img>
+      <img
+        className="main-article__img"
+        src="https://placeimg.com/640/480/tech"
+        alt="img"
+      ></img>
+    </div>
+  );
+};
+
+const Secondary = () => {
+  return <div className="secondary">Drugoplanowy artykul</div>;
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Banner />
+        <MainArticle />
+        <Secondary />
+      </div>
+    );
+  }
 }
 
 export default App;
